ce56591282ffa7140fc1800d3b37a2947fc4788b
Josh Simmons <jds@google.com>
2018-01-02 10:20:27 -0800

    Adding inputrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ln -s dotfiles/.Xdefaults dotfiles/.gitconfig dotfiles/.gvimrc dotfiles/.hgrc dotfiles/.screenrc dotfiles/.tmux.conf dotfiles/.vimrc ~
+ln -s \
+  dotfiles/.Xdefaults \
+  dotfiles/.gitconfig \
+  dotfiles/.gvimrc \
+  dotfiles/.hgrc \
+  dotfiles/.screenrc \
+  dotfiles/.tmux.conf \
+  dotfiles/.vimrc \
+  dotfiles/.inputrc \
+  ~
 
 git clone https://github.com/gmarik/vundle ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
