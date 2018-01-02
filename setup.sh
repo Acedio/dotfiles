d79ff078bcdf3cd92799951f54f4724c471fa333
Josh Simmons <josh.simmons@gmail.com>
2018-01-02 06:26:54 +0000

    Updating vundle install command.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ln -s dotfiles/.Xdefaults dotfiles/.gitconfig dotfiles/.gvimrc dotfiles/.hgrc dotfiles/.screenrc dotfiles/.tmux.conf dotfiles/.vimrc ~
 
-git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 setxkbmap -option "ctrl:nocaps"
