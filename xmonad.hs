48496cf5aeb61f58fe8be5e63739e825074a2344
Josh Simmons <josh.simmons@gmail.com>
2018-01-17 20:27:34 -0800

    No borders fullscreen mode.

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout
+import XMonad.Layout.NoBorders
 import XMonad.Util.Paste
 
 import qualified Data.Map as M
@@ -10,7 +12,7 @@ main = xmonad =<< xmobar defaultConfig
     , modMask     = mod4Mask
     , borderWidth = 1
     , keys = myKeys
-    , layoutHook=avoidStruts $ layoutHook defaultConfig
+    , layoutHook=myLayoutHook
     , manageHook=manageHook defaultConfig <+> manageDocks
     , startupHook = spawn "setxkbmap -option 'ctrl:nocaps' &" >>
                     spawn "killall redshift && redshift -l 47.6494438:-122.3503228 &" >>
@@ -19,6 +21,9 @@ main = xmonad =<< xmobar defaultConfig
 
 -- Union default and new key bindings
 myKeys x  = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
+
+myLayoutHook = (avoidStruts tall ||| avoidStruts Mirror tall ||| noBorders Full)
+                   where tall = Tall 1 0.03 0.5
 
 -- Add new and/or redefine key bindings
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [
